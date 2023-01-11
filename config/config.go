a447b44287fbf5b6990356b91409ad43fb52f739
Chance231412 <Chance231412@3316057884@qq.com>
2023-01-11 16:04:57 +0800

    viper实现配置方案

---
+++ config/config.go
@@ -0,0 +1,5 @@
+package config
+
+// Init 用来触发config包里的所有init
+func Init() {
+}
--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go-oj/bootstrap"
+	"go-oj/config"
+	pkgConfig "go-oj/pkg/config"
 )
 
+func init() {
+	config.Init()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	pkgConfig.InitConfig(env)
+
 	r := gin.New()
 	bootstrap.SetupRoute(r)
-	r.Run(":3000")
+
+	err := r.Run(fmt.Sprintf(":%v", pkgConfig.Get("app.port")))
+	if err != nil {
+		fmt.Println(err)
+	}
 }
